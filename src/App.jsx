c4924cc260e985eb6a9a41d3866d4b5ae48ec78f
Sasa Savic Perusina <sasa@bubulazy.com>
2023-03-06 13:14:25 +0000

    Estructura HTML y CSS  terminada

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <p>Sasa</p>
+    <div className="container">
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h3>Time remaning: 0</h3>
+      <button>START</button>
+      <h2>Word Count: 1</h2>
     </div>
   );
 }
